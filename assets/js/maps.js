80368e65a46c74b83c2e2873c7bc53cde899ebe2
andrius-siup <andrius.siupinys@gmail.com>
2021-03-27 10:56:37 +0000

    Added three markers.

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -41,6 +41,22 @@ function initMap() {
     // new map
     var map = new google.maps.Map(document.getElementById("map"), options);
 
+    addMarker({lat: 42.3601, lng: -71.0589});
+    addMarker({lat: 42.8584, lng: -70.9300});
+    addMarker({lat: 42.7762, lng: -71.0773});
+
+    // add marker function
+    function addMarker(coords) {
+
+        var marker = new google.maps.Marker({
+            // {lat: 42.4668, lng: -70.9495}
+        position:coords,
+        map:map
+    });
+
+    }    
+
+    /**
     // add marker
     var marker = new google.maps.Marker({
         position:{lat: 42.4668, lng: -70.9495},
@@ -54,6 +70,9 @@ function initMap() {
     marker.addListener('click', function() {
         infoWindow.open(map, marker);
     });
+    
+ */
+
 }
 
 
