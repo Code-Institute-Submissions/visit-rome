2ef9c3b9550a735b1a2b75b892204429c6cbe440
andrius-siup <andrius.siupinys@gmail.com>
2021-03-27 11:00:37 +0000

    Create object coords in addMarker, and call later in function attribute position.

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -41,16 +41,16 @@ function initMap() {
     // new map
     var map = new google.maps.Map(document.getElementById("map"), options);
 
-    addMarker({lat: 42.3601, lng: -71.0589});
-    addMarker({lat: 42.8584, lng: -70.9300});
-    addMarker({lat: 42.7762, lng: -71.0773});
+    addMarker({coords:{lat: 42.3601, lng: -71.0589}});
+    addMarker({coords:{lat: 42.8584, lng: -70.9300}});
+    addMarker({coords:{lat: 42.7762, lng: -71.0773}});
 
     // add marker function
-    function addMarker(coords) {
+    function addMarker(props) {
 
         var marker = new google.maps.Marker({
             // {lat: 42.4668, lng: -70.9495}
-        position:coords,
+        position: props.coords,
         map:map
     });
 
