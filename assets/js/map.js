56b993efd4b23e1aa72a2eb42047d3f9b94d885b
andrius-siup <andrius.siupinys@gmail.com>
2021-03-28 19:20:14 +0000

    Added clustering marker.

--- assets/js/map.js
+++ assets/js/map.js
@@ -4,4 +4,13 @@ function initMap() {
         zoom: 10,
         center: {lat:  41.902782, lng: 12.496366}
     };
+    // new map called by id
+    var map = new google.maps.Map(document.getElementById("map"), options);
+    
+    // add marker
+    var marker = new google.maps.Marker({
+        // 41.90135225699609, 12.496885028954154 - Hotel Flower Garden
+        position:{lat: 41.90135225699609, lng: 12.496885028954154},
+        map:map
+    });
 }
