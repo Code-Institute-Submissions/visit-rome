b87fcfbe439cc0e3e3334d8f918bb766c47c1271
andrius-siup <andrius.siupinys@gmail.com>
2021-03-28 20:04:37 +0000

    Added more marker coordinates.

--- assets/js/map.js
+++ assets/js/map.js
@@ -28,6 +28,10 @@ function initMap() {
      */
     
     addMarker({lat: 41.90135225699609, lng: 12.496885028954154});  // 41.90135225699609, 12.496885028954154 - Hotel Flower Garden
+    addMarker({lat:41.903540194120055, lng: 12.493172906843931}); // 41.903540194120055, 12.493172906843931 - Hotel Oceania
+    addMarker({lat: 41.9048177258281, lng: 12.500146775491771}); // 41.9048177258281, 12.500146775491771 - Hotel Windrose
+    addMarker({lat: 41.908123671239544, lng: 12.498108411416945}); // 41.908123671239544, 12.498108411416945 - Marcella Royal Hotel
+    addMarker({lat: 41.908890284572934, lng: 12.493601935040736}); // 41.908890284572934, 12.493601935040736 - Romanico Palace Luxury Hotel & SPA
 
     // add Marker function
     function addMarker(coordinates) {
