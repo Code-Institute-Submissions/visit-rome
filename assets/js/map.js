de478535acd7eeaa46c35796de6a943ecfa8d652
andrius-siup <andrius.siupinys@gmail.com>
2021-03-28 22:16:06 +0000

    Added variable info window, that will be click on the marker will open info window.

--- assets/js/map.js
+++ assets/js/map.js
@@ -2,11 +2,11 @@ function initMap() {
     // variable that store zoom level and coordinates
     var options = {
         zoom: 10,
-        center: {lat:  41.902782, lng: 12.496366}
+        center: { lat: 41.902782, lng: 12.496366 }
     };
     // new map called by id
     var map = new google.maps.Map(document.getElementById("map"), options);
-    
+
     /**
 
     // add marker
@@ -26,19 +26,37 @@ function initMap() {
     });
 
      */
-    
-    addMarker({coords:{lat: 41.90135225699609, lng: 12.496885028954154}});  // 41.90135225699609, 12.496885028954154 - Hotel Flower Garden
-    addMarker({coords:{lat:41.903540194120055, lng: 12.493172906843931}}); // 41.903540194120055, 12.493172906843931 - Hotel Oceania
-    addMarker({coords:{lat: 41.9048177258281, lng: 12.500146775491771}}); // 41.9048177258281, 12.500146775491771 - Hotel Windrose
-    addMarker({coords:{lat: 41.908123671239544, lng: 12.498108411416945}}); // 41.908123671239544, 12.498108411416945 - Marcella Royal Hotel
-    addMarker({coords:{lat: 41.908890284572934, lng: 12.493601935040736}}); // 41.908890284572934, 12.493601935040736 - Romanico Palace Luxury Hotel & SPA
+
+    addMarker({
+        coords: { lat: 41.90135225699609, lng: 12.496885028954154 },  // 41.90135225699609, 12.496885028954154 - Hotel Flower Garden
+        content: '<h3>Hotel Flower Garden</h3>', 
+    });  
+    addMarker({
+        coords: { lat: 41.903540194120055, lng: 12.493172906843931 }  // 41.903540194120055, 12.493172906843931 - Hotel Oceania
+    }); 
+    addMarker({
+        coords: { lat: 41.9048177258281, lng: 12.500146775491771 }  // 41.9048177258281, 12.500146775491771 - Hotel Windrose
+    }); 
+    addMarker({
+        coords: { lat: 41.908123671239544, lng: 12.498108411416945 }  // 41.908123671239544, 12.498108411416945 - Marcella Royal Hotel
+    }); 
+    addMarker({
+        coords: { lat: 41.908890284572934, lng: 12.493601935040736 }  // 41.908890284572934, 12.493601935040736 - Romanico Palace Luxury Hotel & SPA
+    }); 
 
     // add Marker function
     function addMarker(props) {
-         var marker = new google.maps.Marker({
-        position: props.coords,  // props are used to pass a parent component to a child component.
-        map:map,
-    });
+        var marker = new google.maps.Marker({
+            position: props.coords,  // props are used to pass a parent component to a child component.
+            map: map,
+        });
+
+        var infoWindow = new google.maps.InfoWindow({
+            content: props.content
+        });
+        marker.addListener('click', function() {
+            infoWindow.open(map, marker);
+        });
     }
 
 }
