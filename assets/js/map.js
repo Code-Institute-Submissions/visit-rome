cacb11d3a9d01465dcecbc5c1c989454652d2be5
andrius-siup <andrius.siupinys@gmail.com>
2021-03-29 14:32:18 +0000

    Added few comment how to better work on th ecode.

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,8 +1,11 @@
 function initMap() {
     // variable that store zoom level and coordinates
     var options = {
-        zoom: 10,
-        center: { lat: 41.902782, lng: 12.496366 }
+        zoom: 14,
+        center: {
+            lat: 41.902782,
+            lng: 12.496366
+        }
     };
     // new map called by id
     var map = new google.maps.Map(document.getElementById("map"), options);
@@ -27,28 +30,42 @@ function initMap() {
 
      */
 
-    
+
 
     // array of markers
-    var markers = [
-        {
-            coords: { lat: 41.90135225699609, lng: 12.496885028954154 },  // 41.90135225699609, 12.496885028954154 - Hotel Flower Garden
+    var markers = [{
+            coords: {
+                lat: 41.90135225699609,
+                lng: 12.496885028954154
+            }, // 41.90135225699609, 12.496885028954154 - Hotel Flower Garden
             content: '<h3>Hotel Flower Garden</h3>',
         },
         {
-            coords: { lat: 41.903540194120055, lng: 12.493172906843931 },  // 41.903540194120055, 12.493172906843931 - Hotel Oceania
-            content: '<h3>Hotel Oceania</h3>',
+            coords: {
+                lat: 41.903540194120055,
+                lng: 12.493172906843931
+            }, // 41.903540194120055, 12.493172906843931 - Hotel Oceania
+            content: '<h3>Hotel Oceania</h3>', // remove h3
         },
         {
-            coords: { lat: 41.9048177258281, lng: 12.500146775491771 },  // 41.9048177258281, 12.500146775491771 - Hotel Windrose
+            coords: {
+                lat: 41.9048177258281,
+                lng: 12.500146775491771
+            }, // 41.9048177258281, 12.500146775491771 - Hotel Windrose
             content: '<h3>Hotel Windrose</h3>',
         },
         {
-            coords: { lat: 41.908123671239544, lng: 12.498108411416945 },  // 41.908123671239544, 12.498108411416945 - Marcella Royal Hotel
-            content: '<h3>Marcella Royal Hotel</h3>',
+            coords: {
+                lat: 41.908123671239544,
+                lng: 12.498108411416945
+            }, // 41.908123671239544, 12.498108411416945 - Marcella Royal Hotel
+            content: '<h3>Marcella Royal Hotel</h3>', // and remove h3 tag
         },
         {
-            coords: { lat: 41.908890284572934, lng: 12.493601935040736 },  // 41.908890284572934, 12.493601935040736 - Romanico Palace Luxury Hotel & SPA
+            coords: {
+                lat: 41.908890284572934,
+                lng: 12.493601935040736
+            }, // 41.908890284572934, 12.493601935040736 - Romanico Palace Luxury Hotel & SPA
             content: '<h3>Romanico Palace Luxury Hotel & SPA</h3>',
         },
     ];
@@ -60,15 +77,16 @@ function initMap() {
     // add Marker function
     function addMarker(props) {
         var marker = new google.maps.Marker({
-            position: props.coords,  // props are used to pass a parent component to a child component.
+            position: props.coords, // props are used to pass a parent component to a child component.
             map: map,
         });
 
         var infoWindow = new google.maps.InfoWindow({
-            content: props.content
+            content: props.content // content: `<h3>${props.content}</h3>` replace Brian
         });
-        marker.addListener('click', function () {
+        marker.addListener('click', function() {
             infoWindow.open(map, marker);
+            
         });
     }
 
