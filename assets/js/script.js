8884126979f5305e2f7519ea06813ea8e7aab074
andrius-siup <andrius.siupinys@gmail.com>
2021-03-30 12:13:42 +0000

    Moved button text to the jqury.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
 $(document).ready(function() {
-   $(".famouse-places h2").html("Places you must see in Rome");
+   $(".famous-places h2").html("Places you must see in Rome");
+    $(".famous-places-p button").html("Read moere").css('background-color', 'blue');
+
+   
 });
