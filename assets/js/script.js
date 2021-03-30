edf2b17b9e2088bf62ae6aa2f1b5f5f675e58489
andrius-siup <andrius.siupinys@gmail.com>
2021-03-30 18:20:27 +0000

    Added function for famous place button.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
    $(".famous-places h2").html("Places you must see in Rome");
     $(".famous-places-p button").html("Read moere").css('background-color', 'blue');
-
+   
+    $("#btn-famous-one").click(function() {
+    $("#p-famous-one").slideToggle(1000);
+});
    
 });
 
