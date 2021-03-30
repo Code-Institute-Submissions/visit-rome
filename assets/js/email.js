55843018655dcd1a75d87b5849ce9b14ababee94
andrius-siup <andrius.siupinys@gmail.com>
2021-03-30 17:36:59 +0000

    Added modul for contact us.

---
+++ assets/js/email.js
@@ -0,0 +1,9 @@
+
+var myModal = new bootstrap.Modal(document.getElementById('contactUsModal'), options);
+
+
+// var myInput = document.getElementById('myInput')
+
+// myModal.addEventListener('shown.bs.modal', function () {
+//   myInput.focus()
+// })
