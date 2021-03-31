885419dde4d8ce05869d113dc90111c2d988e0af
andrius-siup <andrius.siupinys@gmail.com>
2021-03-31 13:05:06 +0000

    Changed image and text for famous places.

--- assets/js/email.js
+++ assets/js/email.js
@@ -2,12 +2,19 @@
 var myModal = new bootstrap.Modal(document.getElementById('contactUsModal'), options);
 
 
+// var myModal = document.getElementById('contactUsModal');
+// var myInput = document.getElementById('myInput');
 
-emailjs.sendForm('user_Hft1f0wwhp6PvNa61wBb2', 'rome-page-gmail', '#contactUsModal')
-    .then(function(response) {
-       console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-       console.log('FAILED...', error);
-    });
+// myModal.addEventListener('shown.bs.modal', function () {
+//   myInput.focus();
+// })
+
+
+// emailjs.sendForm('user_Hft1f0wwhp6PvNa61wBb2', 'rome-page-gmail', '#contactUsModal')
+//     .then(function(response) {
+//        console.log('SUCCESS!', response.status, response.text);
+//     }, function(error) {
+//        console.log('FAILED...', error);
+//     });
 
 
