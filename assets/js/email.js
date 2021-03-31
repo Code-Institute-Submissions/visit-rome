b756ad928777eb06c7d4749b805180f5cfd144af
andrius-siup <andrius.siupinys@gmail.com>
2021-03-31 11:59:25 +0000

    Adedd function to send form.

--- assets/js/email.js
+++ assets/js/email.js
@@ -2,8 +2,12 @@
 var myModal = new bootstrap.Modal(document.getElementById('contactUsModal'), options);
 
 
-// var myInput = document.getElementById('myInput')
 
-// myModal.addEventListener('shown.bs.modal', function () {
-//   myInput.focus()
-// })
+emailjs.sendForm('user_Hft1f0wwhp6PvNa61wBb2', 'rome-page-gmail', '#contactUsModal')
+    .then(function(response) {
+       console.log('SUCCESS!', response.status, response.text);
+    }, function(error) {
+       console.log('FAILED...', error);
+    });
+
+
